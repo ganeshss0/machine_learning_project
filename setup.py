8898c8c93f47e886ab9f0855de910f28dce0a9cf
Ganesh <gs000@pm.me>
2023-10-11 06:17:26 +0000

    Adding Files

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import find_packages, setup
 
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+
+    with open(file_path) as file:
+        requirements = file.readlines()
+    
+    return [require.strip() for require in requirements if not HYPEN_E_DOT in require]
+
+
+    
 setup(
     name='Simple Machine Learing',
-    packages=find_packages(),
     version='0.1.0',
     description='This is a simple machine learing project.',
     author='Ganesh',
+    author_email='gs000@pm.me',
     license='GNU',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
 )
