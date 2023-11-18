de6c2403186527f141c51fa2e5560895ab27e58e
Ganesh <gs000@pm.me>
2023-11-18 10:22:04 +0000

    Update gitignore and requirements

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import cross_origin, CORS
 app = Flask(__name__)
 CORS(app)
@@ -9,7 +9,7 @@ CORS(app)
 @cross_origin()
 def homepage() -> str:
     '''Render HomePage'''
-    return 'This page is running'
+    return render_template('report.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
