0760e16f45dd8890c449e3a53a2de006eaa67303
Ganesh <gs000@pm.me>
2023-12-02 05:07:21 +0000

    Update Pipeline

--- sample_ml/pipeline/pipeline.py
+++ sample_ml/pipeline/pipeline.py
@@ -6,6 +6,7 @@ from sample_ml.exception import SampleMLException
 from sample_ml.entity.artifact_entity import *
 from sample_ml.entity.config_entity import DataIngestionConfig
 from sample_ml.components.data_ingestion import DataIngestion
+from sample_ml.components.data_validation import DataValidation
 
 
 
@@ -19,21 +20,33 @@ class Pipeline:
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
-            logging.info('Starting Data Ingestion Pipeline')
+            logging.info(f'{"#"*5} Data Ingestion Pipeline Start {"#"*5}')
             data_ingestion = DataIngestion(
                 data_ingestion_config=self.config.get_data_ingestion_config()
             )
-            logging.info('Data Ingestion Pipeline Sucessful')
+            logging.info(f'{"#"*5} Data Ingestion Pipeline Sucessful {"#"*5}')
             return data_ingestion.initiate_data_ingestion()
         except Exception as e:
             logging.error(f'Data Ingestion Pipeline Failed: {e}')
             raise SampleMLException(e, sys) from e
     
-    def start_data_validation(self) -> DataValidationArtifact:
-        pass
+    def start_data_validation(self, data_ingestion_artifact: DataValidationArtifact) -> DataValidationArtifact:
+        try:
+            logging.info(f'{"#"*5} Data Validation Pipeline Start {"#"*5}')
+            data_validation = DataValidation(
+                data_validation_config=self.config.get_data_validation_config(),
+                data_ingestion_artifact=data_ingestion_artifact
+                )
+            logging.info(f'{"#"*5} Data Validation Pipeline Successful {"#"*5}')
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            logging.error(f'Data Validation Pipeline Failed: {e}')
 
     def start_data_transformation(self) -> DataTransformationArtifact:
-        pass
+        try:
+            pass
+        except Exception as e:
+            logging.error(f'Data Transformation Failed: {e}')
 
     def start_model_trainer(self):
         pass
@@ -41,6 +54,8 @@ class Pipeline:
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
+
 
         except Exception as e:
             raise SampleMLException(e, sys) from e
